c8d75c4775fdbe44afd7df15e1bc03e19d973b20
Marcin Siry <sirymarcin@gmail.com>
2023-12-28 22:24:22 +0000

    Dzień 8 - zadanie domowe (Typy referencyjne i wartościowe)

---
+++ ChallengeApp/ChallengeApp.tests/TypeTest.cs
@@ -0,0 +1,66 @@
+﻿namespace ChallengeApp.tests
+{
+    public class TypeTest
+    {
+        [Test]
+        public void TestReferenceType()
+        {
+            // arrange
+            var employee1 = GetEmployee("Marcin", "Nowak", 32);
+            var employee2 = GetEmployee("Marcin", "Kowalski", 27);
+
+            // act
+
+            // assert
+            Assert.AreNotEqual(employee1, employee2);
+
+        }
+
+        [Test]
+        public void TestString()
+        {
+            // arrange
+            var employee1 = GetEmployee("Marcin", "Nowak", 32);
+            var employee2 = GetEmployee("Marcin", "Kowalski", 27);
+
+            // act
+
+            // assert
+            Assert.AreEqual(employee1.Name, employee2.Name);
+
+        }
+
+        [Test]
+        public void TestInt()
+        {
+            // arrange
+            int age1 = 10;
+            int age2 = 10;   
+
+            // act
+
+            // assert
+            Assert.AreEqual(age1, age2);
+
+        }
+
+        [Test]
+        public void TestDifferentTypes()
+        {
+            // arrange
+            double score1 = 10.7;
+            int score2 = 10;
+
+            // act
+
+            // assert
+            Assert.AreEqual(20.7, score1 + score2);
+
+        }
+
+        private Employee GetEmployee(string name, string surname, int age)
+        {
+            return new Employee(name, surname, age);
+        }
+    }
+}
